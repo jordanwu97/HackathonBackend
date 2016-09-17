8dddc6f60bd9d0e1d140a12207bff6102dda74ad
jordanwu97 <Jordan@Jordans-MacBook-Pro.local>
2016-09-17 15:45:54 -0500

    cross origins

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var requireDir = require('require-dir'); //require whole directory
 var cors = require('cors'); //cross origins
+app.use(cors());
 var mongoose = require('mongoose'); //mongoose
   requireDir('./models');
 
