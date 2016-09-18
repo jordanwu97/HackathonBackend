742184eb22dce2505392d61a86932dd320acddc0
muakasan <muakasan@gmail.com>
2016-09-17 19:11:33 -0500

    added image writing

--- routes/imageupload.js
+++ routes/imageupload.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.post('/imageupload', function(req, res, next) {
-  console.log(req);
+  var base64string = req.body.image;
+  //console.log(base64string);
+  //console.log(req.body.hello);
+  console.log(req.body);
+  var bitmap = new Buffer(base64string, 'base64');
+  
+  var directory = 'images/' + req.body.farmerusername + '/' + req.body.inforequestid + '/';
+  
+  fs.writeFileSync('1' + '.jpg' , bitmap);
+  
   res.json(req);
 });
 
